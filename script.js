da373c966ae8dcb3718f030ec69d908e398a504d
Jack Nugent <jacknugent1529@gmail.com>
2021-03-25 23:36:21 -0500

    rendering works properly

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 class Viewer {
-  constructor(canvas, constellations, dir_azi = 0, dir_pol = 0, fov = Math.PI/12,
-              scroll_speed = 1) {
+  constructor(canvas, constellations, dir_azi = 0, dir_pol = 0, fov = Math.PI/12, fov_azi, scroll_speed = 1) {
     this.c = canvas;
     this.constellations = constellations;
 
-    this.dir_azi = dir_azi;
+    this.dir_azi = -dir_azi;
     this.dir_pol = dir_pol;
-    this.fov = fov;
     this.ctx = c.getContext("2d");
     this.scroll_speed = scroll_speed;
 
-    this.aspect = c.width/c.height;
+    this.fov = fov;
     this.distance_to_plane = 1/Math.tan(this.fov);
-    this.fov_azi = Math.atan(this.aspect/this.distance_to_plane);
+
     this.height = window.innerHeight;
     this.width = window.innerWidth;
 
-    this.resize();
+    this.resize(fov_azi);
   }
   
   update() {
@@ -26,16 +24,20 @@ class Viewer {
 
   pinch_zoom(dist1, dist2) {
     this.fov *= dist2/dist1;
-    if (this.fov < Math.PI/20) {
-      this.fov = Math.PI/20;
-    } else if (this.fov > 50*Math.PI/180) {
-      this.fov = 50*Math.PI/180;
-    }
+    this.fov_limits();
     this.distance_to_plane = 1/Math.tan(this.fov);
     this.fov_azi = Math.atan(this.aspect/this.distance_to_plane);
 
     this.update();
 
+  }
+
+  fov_limits() {
+    if (this.fov < Math.PI/20) {
+      this.fov = Math.PI/20;
+    } else if (this.fov > 50*Math.PI/180) {
+      this.fov = 50*Math.PI/180;
+    }
   }
 
   zoom(zoom_in = true, speed = 1) {
@@ -61,7 +63,7 @@ class Viewer {
     this.constellations.forEach(c => c.draw(this));
   }
 
-  resize() {
+  resize(fov_azi=0) {
     this.c.width = window.innerWidth * devicePixelRatio;
     this.c.height = window.innerHeight * devicePixelRatio;
     this.ctx.scale(devicePixelRatio, devicePixelRatio);
@@ -69,16 +71,28 @@ class Viewer {
     this.c.style.height = window.innerHeight + 'px';
     this.aspect = c.width/c.height;
     this.fov_azi = Math.atan(this.aspect/this.distance_to_plane);
+    if (fov_azi != 0 && this.fov_azi < fov_azi) {
+      this.fov = Math.atan(Math.tan(fov_azi)/this.aspect);
+      this.distance_to_plane = 1/Math.tan(this.fov);
+      this.fov_azi = Math.atan(this.aspect/this.distance_to_plane);
+      this.fov_limits();
+    }
+
     this.update();
   }
 
   shiftDir(dx, dy) {
     let dazi_dx = this.fov_azi/this.width;
     let dpolar_dy = this.fov/this.height;
-    this.dir_azi += dazi_dx*dx*2*this.scroll_speed;
+    this.dir_azi -= dazi_dx*dx*2*this.scroll_speed;
     this.dir_pol += dpolar_dy*dy*2*this.scroll_speed;
     this.dir_azi = positive_angle(this.dir_azi);
-    this.dir_pol = positive_angle(this.dir_pol);
+    this.dir_pol = small_angle(this.dir_pol);
+    if (this.dir_pol < -Math.PI/2) {
+      this.dir_pol = -Math.PI/2;
+    } else if (this.dir_pol > Math.PI/2) {
+      this.dir_pol = Math.PI/2;
+    }
     this.update();
   }
 
@@ -91,21 +105,35 @@ class Viewer {
     this.update();
   }
 
+
   sphereToRect(azi, polar) {
+    let c = Math.cos(this.dir_azi);
+    let s = Math.sin(this.dir_azi);
+    let rot_z = [[c, s, 0],
+                 [-s,  c, 0],
+                 [0,  0, 1]];
+    c = Math.cos(this.dir_pol);
+    s = Math.sin(this.dir_pol);
+    let rot_x = [[1, 0,  0],
+                 [0, c, s],
+                 [0, -s,  c]];
+    let [x_c,y_c,z_c] = sphereToCartesian([1, azi, polar]);
+    let [x2_arr,y2_arr,z2_arr] = matmul(rot_x, matmul(rot_z, [[x_c],[y_c],[z_c]]));
+    let [_, azi2, polar2] = cartesianToSphere([x2_arr[0], y2_arr[0], z2_arr[0]]);
+
     let in_fov = true;
-    if (small_angle(polar - this.dir_pol) > this.fov ||
-        small_angle(polar - this.dir_pol) < -this.fov ||
-        small_angle(azi - this.dir_azi) > this.fov_azi ||
-        small_angle(azi - this.dir_azi) < -this.fov_azi) {
+    if (small_angle(azi2) > this.fov_azi || small_angle(azi2) < -this.fov_azi ||
+        small_angle(polar2) > this.fov   || small_angle(polar2) < -this.fov ||
+        y2_arr[0] < 0) {
       in_fov = false;
     }
 
-    let y = this.distance_to_plane * Math.tan(this.dir_pol - polar);
-    y = (y + 1)/2 * this.height;
+    let y = this.distance_to_plane * Math.tan(polar2);
+    y = (-y + 1)/2 * this.height;
 
-    let x = this.distance_to_plane * Math.tan(this.dir_azi - azi);
+    let x = this.distance_to_plane * Math.tan(azi2);
     x /= this.aspect;
-    x = (x + 1)/2 * this.width;
+    x = (-x + 1)/2 * this.width;
     return [x,y, in_fov];
   }
 
@@ -119,39 +147,11 @@ class Viewer {
     }
   }
 
-  thetaWithinRange(theta1, theta2, dir, fov) {
-    theta1 -= dir;
-    theta2 -= dir;
-    theta1 = positive_angle(theta1);
-    theta2 = positive_angle(theta2);
-    if (theta1 > theta2) {
-      let tmp = theta1;
-      theta1 = theta2;
-      theta2 = tmp;
-    }
-
-    theta1 = small_angle(theta1);
-    theta2 = small_angle(theta2);
-
-    if (theta1 > 0 && theta2 > 0) {
-      return theta1 < fov;
-    } else if (theta1 < 0 && theta2 < 0) {
-      return theta2 > -fov;
-    } else {
-      return theta1 < Math.PI/2 && theta2 > -Math.PI/2
-    }
-
-  }
-
-  lineVisible(azi1, pol1, azi2, pol2) {
-    return this.thetaWithinRange(azi1, azi2, this.dir_azi, this.fov_azi)
-        && this.thetaWithinRange(pol1, pol2, this.dir_pol, this.fov);
-  }
 
   drawLine(azi1, polar1, azi2, polar2, width=2.5) {
-    let [x1,y1] = this.sphereToRect(azi1, polar1);
-    let [x2,y2] = this.sphereToRect(azi2, polar2);
-    if (!this.lineVisible(azi1, polar1, azi2, polar2)) {
+    let [x1,y1,in_fov1] = this.sphereToRect(azi1, polar1);
+    let [x2,y2,in_fov2] = this.sphereToRect(azi2, polar2);
+    if (!(in_fov1 || in_fov2)) {
       return;
     }
 
@@ -175,27 +175,18 @@ class Viewer {
 }
 
 class Constellation {
-  constructor(stars, lines, name, abbreviation) {
+  constructor(stars, lines, name, abbreviation, middle_star, bounds) {
     // lines are the indices of the stars i.e. [0,1] line from star 0 to star 1
     this.stars = stars;
     this.lines = lines;
     this.name = name;
     this.abbreviation = abbreviation;
-    this.middle_star = null;
-    this.fov = null;
-    this.calc();
-    this.dir_azi = this.middle_star.azi;
-    this.dir_pol = this.middle_star.polar;
+    this.middle_star = this.stars[middle_star];
+    this.middle_star.label = this.name;
+    this.middle_star.hasLabel = true;
+    this.fov = bounds[1];
   }
 
-  calc(label_middle_star = true) {
-    this.middle_star = this.stars[0];
-    this.fov = Math.PI/6;
-    if (label_middle_star) {
-      this.middle_star.label = this.name;
-      this.middle_star.hasLabel = true;
-    }
-  }
 
   draw(viewer) {
     this.stars.forEach(s => s.draw(viewer));
@@ -239,14 +230,58 @@ function small_angle(theta) {
   return theta;
 }
 
-function json_to_constellation(obj) {
-  stars = obj.stars.map(point => new Star(point[0], point[1]));
-  return new Constellation(stars, obj.lines, obj.name, obj.abb);
+// [[1,2,3],
+//  [4,5,6],
+//  [7,8,9]]
+function matmul(a,b) {
+  if (a[0].length != b.length) {
+    throw 'Dimension error';
+  }
+
+  out = []
+  for(let i = 0; i < a.length; i++) {
+    row = []
+    for(let j = 0; j < b[0].length; j++) {
+      let sum = 0;
+      for(let k = 0; k < a[0].length; k++) {
+        sum += a[i][k]*b[k][j];
+      }
+      row.push(sum);
+    }
+    out.push(row);
+  }
+  return out;
 }
 
-function parseJSON(json) {
-  jsonData = JSON.parse(json);
-  return jsonData.map(json_to_constellation);
+let a =  [[1,2,3],
+          [4,5,6],
+          [7,8,9]];
+
+let b =  [[7,8,9],
+          [1,2,3],
+          [4,5,6]];
+
+
+function sphereToCartesian([r, azi, pol]) {
+  return [r*Math.cos(pol)*Math.sin(azi),
+          r*Math.cos(pol)*Math.cos(azi),
+          r*Math.sin(pol)];
+}
+
+function cartesianToSphere([x,y,z]) {
+  let r = Math.sqrt(x**2+y**2+z**2);
+  let azi = Math.atan(x/y);
+  let pol = Math.asin(z/r);
+  return [r, azi, pol];
+}
+
+function json_to_constellation(obj) {
+  stars = obj.stars.map(point => new Star(point[0], point[1]));
+  return new Constellation(stars, obj.lines, obj.name, obj.abb, obj.middle, obj.bounds);
+}
+
+function parseJSONdict(dict) {
+  return Object.entries(dict).map(entry => json_to_constellation(entry[1]));
 }
 
 let c = document.getElementById("canvas");
@@ -254,8 +289,20 @@ c.setAttribute("tabindex", 0);
 c.focus();
 
 
-constellations = parseJSON(constellations_json);
-viewer = new Viewer(c, constellations, 1.4417, 0.061, 0.31733, 1);
+constellations_dict = JSON.parse(constellations_json);
+constellations_list = parseJSONdict(constellations_dict);
+
+let focus_abb = window.location.href.split('html')[1].slice(1);
+let focus = constellations_dict[focus_abb];
+if (focus == undefined) {
+  focus = constellations_dict['Ori']
+}
+let dir_azi = focus.stars[focus.middle][0];
+let dir_pol = focus.stars[focus.middle][1];
+let fov = focus.bounds[1]+2*Math.PI/180;
+let fov_azi = focus.bounds[0]+2*Math.PI/180;
+
+viewer = new Viewer(c, constellations_list, dir_azi, dir_pol, fov, fov_azi, 1);
 
 
 //controls
