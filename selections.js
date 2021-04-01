0d6bb116492ce25a9a644577d8cf9e687472aac9
Jack Nugent <jacknugent1529@gmail.com>
2021-04-01 15:50:53 -0500

    added filters

---
+++ selections.js
@@ -0,0 +1,60 @@
+categories = JSON.parse(categories_json);
+const checkboxes = document.getElementsByClassName('category-checkbox');
+for (let con in categories) {
+  categories[con]['visible'] = true;
+}
+let totalVisible = 88;
+
+const num_visible = document.getElementById('num-constelations-visible');
+
+function togglePropertyVis(check_id, property, value) {
+  let checked = document.getElementById(check_id).checked;
+  for (let con in categories) {
+    let con_value = categories[con][property];
+    if (con_value == value) {
+      if (checked) {
+        changeVis(con, true);
+      } else {
+        changeVis(con, false);
+      }
+    }
+  }
+  updateNumVisibleDisplay();
+}
+
+function updateNumVisibleDisplay() {
+  num_visible.innerHTML = totalVisible;
+}
+
+function changeVis(con, vis=true) {
+  if (categories[con]['visible'] != vis) {
+    categories[con]['visible'] = vis;
+    if (vis) {
+      totalVisible++;
+      document.getElementById(con).classList.remove('d-none');
+    } else {
+      totalVisible--;
+      document.getElementById(con).classList.add('d-none');
+    }
+  }
+}
+
+function selectAll() {
+  for (let elm of checkboxes) {
+    elm.checked = true;
+  }
+  for (let con in categories) {
+    changeVis(con, true);
+  }
+  updateNumVisibleDisplay();
+}
+
+function deselectAll() {
+  for (let elm of checkboxes) {
+    elm.checked = false;
+  }
+  for (let con in categories) {
+    changeVis(con, false);
+  }
+  updateNumVisibleDisplay();
+}
